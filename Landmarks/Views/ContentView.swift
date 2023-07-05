cf34bcf731c752189a7e69d6c699163b718416be
Kevin Marin <kevinmarinsaavedra@gmail.com>
2023-07-05 16:36:52 +0200

    Building Lists and Navigation

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,20 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Kevin Marin on 5/7/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        LandmarkList()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
