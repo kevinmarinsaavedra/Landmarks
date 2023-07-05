b5f0c7078a8d507d1a9173fcc4365dd3d78de478
Kevin Marin <kevinmarinsaavedra@gmail.com>
2023-07-05 13:45:49 +0200

    Creating and Combining Views

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Kevin Marin on 5/7/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
